82849b430ec4072bdfce4361feefe044ab398216
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-04-24 19:42:32 +0200

    codealong

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Card } from 'lib/Card'
+
+const ThinnerCard = styled(Card)`
+  width: 400px;
+`
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <ThinnerCard
+        coverImage="https://picsum.photos/500/300"
+        thumbnailUrl="https://picsum.photos/100/100"
+        title="This is the title"
+        secondaryText="Secondary title">
+          Child content
+      </ThinnerCard>
+
+      <Card title="Only a title" secondaryText="And second" />
+
+      <Card>
+        <h1>Hello from children</h1>
+      </Card>
+
     </div>
   )
 }
